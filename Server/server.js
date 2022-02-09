df3e5ac397aab69eb8d320b1712d8665638e2a25
Juan Tavera <juanskysword@gmail.com>
2022-02-09 12:22:04 -0800

    Start3

--- Server/server.js
+++ Server/server.js
@@ -12,6 +12,7 @@ app.use(express.static("public"));
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, '../public/inex.html'))
 })
+
 const port = process.envPORT || 4000
 
 app.listen(4000, () => console.log(`server running on 4000`))
